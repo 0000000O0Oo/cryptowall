a097ed84689bcdc80b9b19dfb91ebbc1cb504b11
Marco Wang <m.aesophor@gmail.com>
2019-04-11 23:06:49 +0800

    clean up main

--- src/main.cc
+++ src/main.cc
@@ -5,17 +5,15 @@
 #include "file_encryptor.h"
 
 using std::cout;
-using std::cerr;
 using std::endl;
 using std::vector;
 using std::string;
 
-int main() {
+int main(int argc, char* args[]) {
   FileEncryptor encryptor("/home/aesophor/Temp");
   vector<string> files = encryptor.ListDirectory();
 
   for (const auto& file : files) {
-    string new_name = file + ".fuxsc";
     cout << "encrypting: " << file << endl;
     encryptor.Encrypt(file);
     encryptor.Decrypt(file + ".fuxsc");
